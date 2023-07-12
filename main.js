18b2a587b6036ff74d9c72a34518f72d95b875a2
Your Name <nayu.miffy@gmail.com>
2023-07-12 15:15:07 +0900

    コメントアウト

--- main.js
+++ main.js
@@ -2,21 +2,24 @@ Vue.createApp({
   data: function () {
     return {
       member: ["佐藤", "伊藤", "遠藤", "鈴木", "田中", "小林", "千葉", "加藤", "相澤", "中村", "高橋", "渡辺", "山本", "吉田", "石田"]
-      newMember: ""
     }
   },
   methods: {
     btnClick: function () {
-      this.member;
+      random(this.member, 6)
     }
   },
   computed: {
-    random: function () {
-        let newMember = []
-        this.member.forEach(num => {
-          newMember.push(this.member[Math.floor(Math.random() * num)])
-        })
-      return newMember
+    random: function (arrayData, count) {
+      var count = count || 1;
+      var arrayData = arrayData;
+      let newMember = []
+      for (var i = 0; i < count; i++) {
+        var arrayIndex = Math.floor(Math.random() * arrayData.length);
+        newMember[i] = arrayData[arrayIndex];
+        arrayData.splice(arrayIndex, 1);
       }
+      return newMember;
+    }
   }
 }).mount("#app");
