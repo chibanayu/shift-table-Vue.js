2dd93bff299b8c0625f1ac2795ef84293b3d4904
Your Name <nayu.miffy@gmail.com>
2023-07-18 14:05:18 +0900

    シャッフル関数追加

--- main.js
+++ main.js
@@ -1,40 +1,24 @@
 Vue.createApp({
   data: function () {
     return {
-      members: ["佐藤", "伊藤", "遠藤", "鈴木", "田中", "小林", "千葉", "加藤", "相澤", "中村", "高橋", "渡辺", "山本", "吉田", "石田"],
-      array: ""
+      array: ["佐藤", "伊藤", "遠藤", "鈴木", "田中", "小林", "千葉", "加藤", "相澤", "中村", "高橋", "渡辺", "山本", "吉田", "石田"],
+      randomArray: []
     }
   },
+  mounted() {
+    this.randomizeArray();
+  },
   computed: {
-    random: function () {
-      let array = []
-      this.members.forEach(name => {
-        array.push(this.members[Math.floor(Math.random() * name)])
-      })
-      return array
+    randomizeArray() {
+      // 配列をランダムにシャッフルする関数
+      const shuffleArray = array => {
+        for (let i = array.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+      }
+      this.randomArray = shuffleArray(this.array);
     }
   }
 }).mount("#app")
-
-// random: function (arrayData, count) {
-//   //countが設定されていない場合は1にする
-//   var count = count || 1;
-//   var arrayData1 = arrayData;
-//   //新しい配列を用意
-//   let array = []
-//   for (var i = 0; i < count; i++) {
-//     var arrayIndex = Math.floor(Math.random() * arrayData1);
-//     array[i] = arrayData1[arrayIndex];
-//     //1回選択された値は削除して再度選ばれないようにする
-//     arrayData1.splice(arrayIndex, 1);
-//   }
-//   //配列に戻す
-//   return array
-// }
-// }
-
-// random: function () {
-// return random(this.members, 6);
-// console.log(this.members)
-// }
-// }
