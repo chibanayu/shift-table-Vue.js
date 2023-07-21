5f8b045daf5805f341c48f1489790b563db06f5d
Your Name <nayu.miffy@gmail.com>
2023-07-21 15:13:45 +0900

    ボタンが押された時の処理を追加

--- main.js
+++ main.js
@@ -1,24 +1,42 @@
 Vue.createApp({
   data: function () {
     return {
-      array: ["佐藤", "伊藤", "遠藤", "鈴木", "田中", "小林", "千葉", "加藤", "相澤", "中村", "高橋", "渡辺", "山本", "吉田", "石田"],
-      randomArray: []
+      names: ["佐藤", "伊藤", "遠藤", "鈴木", "田中", "小林", "千葉", "加藤", "山崎", "中村", "高橋", "渡辺", "山本", "吉田", "石田", "佐藤", "伊藤", "遠藤", "鈴木", "田中", "小林", "千葉", "加藤", "山崎", "中村", "高橋", "渡辺", "山本", "吉田", "石田"],
+      names6: [],
     }
+  },
+  methods: {
+    //ボタンが押された時の処理
+    buttonClick() {
+      //4週分繰り返す
+      for (let i = 0; i < 4; i++) {
+        //月~金をdaysOfWeekに格納
+        const daysOfWeek = ["月", "火", "水", "木", "金"];
+      //namesの配列からデータの一部分だけ取り出しnamesCopyに格納
+      const namesCopy = this.names.slice();
+        const weeks = 2;
+      //weeksを繰り返す
+        for (let i = 0; i < weeks; i++) {
+        const weekSchedule = { weekNumber: i + 1, days: [] };
+        daysOfWeek.forEach((day) => {
+          const daySchedule = { dayName: day, names: [] };
+          //6回繰り返す
+          for (let j = 0; j < 6; j++) {
+            if (namesCopy.length === 0) break;
+            //namesCopyの要素数を取得→ランダム→randomIndexに格納
+            const randomIndex = Math.floor(Math.random() * namesCopy.length);
+            daySchedule.names.push(namesCopy.splice(randomIndex, 1)[0]);
+          }
+          weekSchedule.days.push(daySchedule);
+        });
+        //names6という空の配列に追加する
+        this.names6.push(weekSchedule);
+      }
+    }
+    },
   },
   mounted() {
-    this.randomizeArray();
-  },
-  computed: {
-    randomizeArray() {
-      // 配列をランダムにシャッフルする関数
-      const shuffleArray = array => {
-        for (let i = array.length - 1; i > 0; i--) {
-          const j = Math.floor(Math.random() * (i + 1));
-          [array[i], array[j]] = [array[j], array[i]];
-        }
-        return array;
-      }
-      this.randomArray = shuffleArray(this.array);
+    for (let i = 0; i < buttonClick(); i++) {
     }
-  }
+  },
 }).mount("#app")
